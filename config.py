ff6f27824f97a38ce3c9c0fe1d2661365b851e5a
jeevvanth <jeevanthbheeman@gmail.com>
2026-01-19 15:42:26 +0530

    ft:changes in the config

--- config.py
+++ config.py
@@ -19,9 +19,9 @@ SAMPLE_RATE = 24000
 CHANNEL=1
 BLOCK=480
 
-LIVEKIT_URL="wss://productdemoagent-lfuy3gq4.livekit.cloud"
-LIVEKIT_API_KEY="API95ZMNGmcRMXr"
-LIVEKIT_API_SECRET="XL0YiensmiBJMqtI7hXwgpi9uLGufOefYJZPp7O9AmaD"
+LIVEKIT_URL=os.getenv('LIVEKIT_URL')
+LIVEKIT_API_KEY=os.getenv('LIVEKIT_API_KEY')
+LIVEKIT_API_SECRET=os.getenv('LIVEKIT_API_SECRET')
 
 agent = RealtimeAgent(
     name="OnCreate Demo Agent",
